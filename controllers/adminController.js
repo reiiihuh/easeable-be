cf7e6f5e152a3c7c8a5f52dcb84e244e70bf0261
reiiihuh <reimitsune@gmail.com>
2025-06-18 12:12:22 +0700

    Update 18/06/25

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt';
 import { db } from '../models/db.js';
+import jwt from 'jsonwebtoken';
 
 export const registerAdmin = (req, res) => {
   const { nama, username, email, password } = req.body;
@@ -23,4 +24,28 @@ export const registerAdmin = (req, res) => {
 
     res.json({ message: 'Admin berhasil didaftarkan' });
   });
+};
+
+export const loginAdmin = (req, res) => {
+    const { username, password } = req.body;
+  
+    // // Validasi email
+    // if (!email.endsWith('@student.telkomuniversity.ac.id')) {
+    //   return res.status(400).json({ message: 'Gunakan email SSO Telkom University.' });
+    // }
+  
+    const sql = 'SELECT * FROM admin WHERE username = ?';
+    db.query(sql, [username], async (err, result) => {
+      if (err) return res.status(500).json({ error: err });
+  
+      if (result.length === 0) return res.status(401).json({ message: 'Username tidak ditemukan.' });
+  
+      const user = result[0];
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (!passwordMatch) return res.status(401).json({ message: 'Password salah.' });
+  
+      const token = jwt.sign({ id_admin: user.id_admin, role: 'admin' }, process.env.JWT_SECRET, { expiresIn: '2h' });
+  
+      res.json({ token, user: { id_admin: user.id_admin, name: user.nama, email: user.email } });
+    });
 };
--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { registerAdmin } from '../controllers/adminController.js';
+import { loginAdmin } from '../controllers/adminController.js';
 
 const router = express.Router();
 
 router.post('/register', registerAdmin);
+router.post('/login', loginAdmin);
 
 export default router;
